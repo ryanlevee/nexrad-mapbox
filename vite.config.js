927cdbe4abfa250c9d17fb41d3cd4413a3c0a600
ryanlevee <ryanlevee@gmail.com>
2025-03-16 22:36:35 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import dotenv from 'dotenv'
+dotenv.config()
 
 export default defineConfig({
     plugins: [solidPlugin()],
@@ -10,6 +12,6 @@ export default defineConfig({
         target: 'esnext',
     },
     define: {
-        'import.meta.env.VITE_MAPBOX_ACCESS_TOKEN': JSON.stringify(import.meta.env.VITE_MAPBOX_ACCESS_TOKEN)
+        'import.meta.env.VITE_MAPBOX_ACCESS_TOKEN': JSON.stringify(process.env.VITE_MAPBOX_ACCESS_TOKEN)
     }
 });
